2a6cc058c244e075251335cdf6bb940ba2737106
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-09 19:20:17 +0300

    squash!

--- Target/Function/Generate.js
+++ Target/Function/Generate.js
@@ -1,1 +1,1 @@
-var s=(r,t,n)=>{const o=[];return r.forEach(async a=>{const{Changed:g,Text:h,Change:e}=(await import("./Replace.js")).default(a,t,n);g&&e.push({File:a,Text:h,Change:e})}),o};export{s as default};
+var h=(e,t,n)=>{const o=[];return e.forEach(async a=>{const{Changed:s,Text:g,Change:r}=(await import("./Replace.js")).default(a,t,n);s&&r.push({File:a,Text:g,Change:r})}),o};export{h as default};
